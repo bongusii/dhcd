73e3b146f0fda381e08b3ef5fe6b6ba59c6bf6ec
bongusii <bongusi9124@gmail.com>
2025-09-13 12:31:19 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+// Vui lòng thay thế các giá trị dưới đây bằng thông tin cấu hình Firebase của bạn.
+// Bạn có thể lấy thông tin này từ "Project settings" trong Firebase console.
+export const firebaseConfig = {
+  apiKey: "AIzaSyCCdvxFNc9o5l103zSOJ4nbjxQxmRxAyec",
+  authDomain: "dhcd-38cd2.firebaseapp.com",
+  projectId: "dhcd-38cd2",
+  storageBucket: "dhcd-38cd2.firebasestorage.app",
+  messagingSenderId: "868300885105",
+  appId: "1:868300885105:web:597da79beb06d13d8a5fd0",
+  measurementId: "G-637PY8ZPM9"
+};
+
+
+// Vui lòng đặt tài khoản và mật khẩu admin tại đây.
+// **LƯU Ý QUAN TRỌNG**: Đây là phương pháp đăng nhập đơn giản, không an toàn cho môi trường production.
+// Để bảo mật hơn, hãy cân nhắc sử dụng hệ thống Firebase Authentication.
+export const adminCredentials = {
+    username: "bongusii",
+    password: "09012004"
+};
